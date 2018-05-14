3bfb4baa31082bc43c794c82f47f185812d16abc
simdi <sim04ful@gmail.com>
2018-05-14 14:17:48 +0100

    yep done

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ config.server = {
 }
 
 config.neo4j = {
-  host: process.env.NEO4J_HOST || 'bolt://localhost',
+  host: process.env.NEO4J_HOST || 'bolt://206.187.199.187',
   port: process.env.NEO4J_PORT || 7687,
   username: process.env.NEO4J_USERNAME || 'neo4j',
   password: process.env.NEO4J_PASSWORD || 'miriamisnice'
--- src/neo4j.connector.test.js
+++ src/neo4j.connector.test.js
@@ -5,17 +5,17 @@ import _ from 'lodash'
 const neo4j = new Neo4jGraphql()
 
 test('getMedia should return a promise', () => {
-  let media = neo4j.getMedia('Taya', 'all', {page: 1, limit: 5})
+  let media = neo4j.getMedia('Taya', 'all', { page: 1, limit: 5 })
   expect(isPromise(media)).toBe(true)
 })
 
-test('Resolved getMedia should return array', async() => {
-  let media = await neo4j.getMedia('Taya', 'all', {page: 1, limit: 5})
+test('Resolved getMedia should return array', async () => {
+  let media = await neo4j.getMedia('Taya', 'all', { page: 1, limit: 5 })
   expect(media).toBeInstanceOf(Array)
 })
 
-test('Resolved getMedia array items should be an object with correct attributes', async() => {
-  let media = await neo4j.getMedia('Taya', 'all', {page: 1, limit: 5})
+test('Resolved getMedia array items should be an object with correct attributes', async () => {
+  let media = await neo4j.getMedia('Taya', 'all', { page: 1, limit: 5 })
 
   expect(media[0]).toHaveProperty('name')
   expect(media[0]).toHaveProperty('id')
@@ -24,9 +24,9 @@ test('Resolved getMedia array items should be an object with correct attributes'
   expect(media[0]).toHaveProperty('url')
 })
 
-test('pages should not have repeat items', async() => {
-  let media_page1 = await neo4j.getMedia('Taya', 'all', {page: 1, limit: 5})
-  let media_page2 = await neo4j.getMedia('Taya', 'all', {page: 2, limit: 5})
+test('pages should not have repeat items', async () => {
+  let media_page1 = await neo4j.getMedia('Taya', 'all', { page: 1, limit: 5 })
+  let media_page2 = await neo4j.getMedia('Taya', 'all', { page: 2, limit: 5 })
 
   let pages1_length = media_page1.length
   let pages1_unique_length = _.uniqBy(media_page1, 'name').length
