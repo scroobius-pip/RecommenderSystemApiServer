d182c996d7ce277d20a9bac4ca529dff85a5becf
simdi <sim04ful@gmail.com>
2018-05-20 01:09:16 +0100

    changed config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ config.server = {
 }
 
 config.neo4j = {
-  host: process.env.NEO4J_HOST || 'bolt://206.187.199.187',
+  host: process.env.NEO4J_HOST || 'bolt://206.189.199.187',
   port: process.env.NEO4J_PORT || 7687,
   username: process.env.NEO4J_USERNAME || 'neo4j',
   password: process.env.NEO4J_PASSWORD || 'miriamisnice'
